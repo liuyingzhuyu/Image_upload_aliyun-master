eefc2e9bfa50dc5d916a44e642260ef89c91af29
jinghua@blchina.com <13161166589@163.com>
2019-10-14 15:17:49 +0800

    小程序向阿里云服务器上传图片

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Image_upload_aliyun
+微信小程序上传图片到阿里云oss
+
+我把主要注释都写在js文件里了，相信你们都看的懂哈！！！
+
+
+出处：https://github.com/hujinchen/Image_upload_aliyun
---
+++ utils/config.js
@@ -0,0 +1,21 @@
+// var fileHost = "https://xxx/";//你的阿里云地址最后面跟上一个/   在你当前小程序的后台的uploadFile 合法域名也要配上这个域名
+// var config = {
+//    //aliyun OSS config
+//    uploadImageUrl: `${fileHost}`, // 默认存在根目录，可根据需求改
+//    AccessKeySecret: 'xxx',        // AccessKeySecret 去你的阿里云上控制台上找
+//    OSSAccessKeyId: 'xxx',         // AccessKeyId 去你的阿里云上控制台上找
+//    timeout: 87600 //这个是上传文件时Policy的失效时间
+// };
+// module.exports = config
+
+
+var fileHost = "https://moby-oss-dev.oss-cn-beijing.aliyuncs.com/";//你的阿里云地址最后面跟上一个/   在你当前小程序的后台的uploadFile 合法域名也要配上这个域名
+var config = {
+   //aliyun OSS config
+   uploadImageUrl: `${fileHost}`, // 默认存在根目录，可根据需求改
+   AccessKeySecret: 'WoNjdktSwLGSEQAsxYWkMkALUuz2CI',        // AccessKeySecret 去你的阿里云上控制台上找
+   OSSAccessKeyId: 'LTAI1agzaBG5gXHe',         // AccessKeyId 
+	 OSSDIR: 'image/platform/',// 上传目录
+   timeout: 87600 //这个是上传文件时Policy的失效时间
+};
+module.exports = config
